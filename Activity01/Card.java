489028326ac8cecf83e290d0690d6a9acb4b398d
grant.sokolowski@mrjaffesclass.com <grant.sokolowski@mrjaffesclass.com>
2015-04-13 11:32:45 -0700

    Activity1,2,and3

--- Activity01/Card.java
+++ Activity01/Card.java
@@ -22,8 +22,7 @@ public class Card {
      */
     private int pointValue;
 
-
-   /**
+    /**
      * Creates a new <code>Card</code> instance.
      *
      * @param cardRank  a <code>String</code> value
@@ -34,16 +33,17 @@ public class Card {
      *                  containing the point value of the card
      */
     public Card(String cardRank, String cardSuit, int cardPointValue) {
-
+        this.suit = cardSuit;
+        this.rank = cardRank;
+        this.pointValue = cardPointValue;
     }
-
 
     /**
      * Accesses this <code>Card's</code> suit.
      * @return this <code>Card's</code> suit.
      */
     public String suit() {
-
+        return suit;
     }
 
     /**
@@ -51,15 +51,15 @@ public class Card {
      * @return this <code>Card's</code> rank.
      */
     public String rank() {
-
+        return rank;
     }
 
-   /**
+    /**
      * Accesses this <code>Card's</code> point value.
      * @return this <code>Card's</code> point value.
      */
     public int pointValue() {
-
+        return pointValue;
     }
 
     /** Compare this card with the argument.
@@ -69,7 +69,19 @@ public class Card {
      *         false otherwise.
      */
     public boolean matches(Card otherCard) {
-
+        if(rank==otherCard.rank()){
+            if(suit==otherCard.suit()){
+                if(pointValue==otherCard.pointValue()){
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
     }
 
     /**
@@ -84,6 +96,7 @@ public class Card {
      */
     @Override
     public String toString() {
-
+        return (rank+" of "+suit+"(point value = "+pointValue+")"); 
+       
     }
 }
